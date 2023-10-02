454cd394268a3b92d9740f7414b6304a69adf5ea
mehdad <mehrdad.mohammadi@digikala.com>
2023-10-02 22:11:54 +0330

    Add models and migrations

--- database/migrations/2023_10_02_183926_create_sms_gateways_table.php
+++ database/migrations/2023_10_02_183926_create_sms_gateways_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sms_gateways', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('api_key');
+            $table->string('api_secret');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sms_gateways');
     }
 };
