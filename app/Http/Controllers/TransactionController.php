eb57afa1f16d4ff77ff2c9d88c07886be8a64f6b
mehdad <mehrdad.mohammadi@digikala.com>
2023-10-03 10:48:42 +0330

    Add sample seeder

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -11,7 +11,6 @@ class TransactionController extends Controller
 {
     public function moveBalance(Request $request, TransactionService $transactionService, NotificationService $notificationService)
     {
-        // Validate the request
         $request->validate([
             'source_card_number' => 'required',
             'destination_card_number' => 'required',
@@ -20,19 +19,15 @@ class TransactionController extends Controller
 
         DB::beginTransaction();
         try {
-            // Use the TransactionService to move the balance
             $transaction = $transactionService->moveBalance($request->source_card_number, $request->destination_card_number, $request->amount);
 
-            // Use the NotificationService to send notifications
             $notificationService->sendTransactionNotification($transaction);
 
             DB::commit();
 
-            // Return a success response
             return response()->json(['success' => 'Balance moved successfully']);
         } catch (\Exception $e) {
             DB::rollback();
-            // Return an error response
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
